3ee83e44338e732462fe89aeed82fc7345c1021e
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-27 11:00:51 -0300

    Feat: update theme

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2726,10 +2726,10 @@ var useKonamiCheatCode = function (matchedCodeHandler) {
 
 var baseColors = {
     failure: "#ED4B9E",
-    primary: "#1FC7D4",
+    primary: "#9a6aff",
     primaryBright: "#53DEE9",
     primaryDark: "#0098A1",
-    secondary: "#7645D9",
+    secondary: "#8358e6",
     success: "#31D0AA",
     warning: "#FFB237",
 };
